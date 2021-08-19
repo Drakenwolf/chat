eae74c0e7907aeea6eed699850ec32fc76013f43
Drakenwolf <you@example.comhans.haar6@gmail.com>
2021-08-18 19:48:18 -0600

    last functions

--- server/index.js
+++ server/index.js
@@ -27,6 +27,11 @@ io.on("connection", (socket) => {
       .emit("message", { user: "admin", text: `${user.name} has join` });
     socket.join(user.room);
 
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      user: getUserInRoom(user.room),
+    });
+
     callback();
   });
 
@@ -34,12 +39,22 @@ io.on("connection", (socket) => {
     const user = getUser(socket.id);
 
     io.to(user.room).emit("message", { user: user.name, text: message });
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUserInRoom(user.room),
+    });
 
     callback();
   });
 
   socket.on("disconnect", () => {
-    console.log("user has gone");
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("message", {
+        user: "admin",
+        text: `${user.name} has left`,
+      });
+    }
   });
 });
 
